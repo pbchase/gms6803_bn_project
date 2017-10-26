49d21361aa5e27192a07ba939370c01fcbe1b407
Philip Chase <pbc@ufl.edu>
2017-10-25 21:58:51 -0400

    Add irregular margin (im) to the bayes network code

--- src/tcaBN.py
+++ src/tcaBN.py
@@ -20,9 +20,9 @@ def main():
 
     # Add nodes
     mc = Node("mc")
-    #cy = Node("Cystic aspect")
-    #pv = Node("Peripheral vascularity")
+    im = Node("im")
     he = Node("he")
+    #pv = Node("Peripheral vascularity")
     #rs = Node("Round shape")
 
     ca = Node("ca")
@@ -30,16 +30,16 @@ def main():
     kw = Node("kw")
 
     # enumerate those nodes
-    nodes = [mc, he, ca, op, kw]
+    nodes = [mc, im, he, ca, op, kw]
 
     # Add outcomes for each node
     [ x.addOutcomes(['yes', 'no']) for x in nodes]
 
     # Add edges between nodes
     arc_mc_ca = Arc(mc,ca)
-    # arc_cy_ca = Arc(cy,ca)
-    # arc_pv_ca = Arc(pv,ca)
+    arc_im_ca = Arc(im,ca)
     arc_he_ca = Arc(he,ca)
+    # arc_pv_ca = Arc(pv,ca)
     # arc_rs_ca = Arc(rs,ca)
 
     arc_ca_op = Arc(ca,op)
@@ -49,15 +49,15 @@ def main():
     # from Kim et al, 2002, "New Sonographic Criteria for Recommending Fine-Needle Aspiration Biopsy of Nonpalpable Solid Nodules of the Thyroid"
     # 155 non-palpable nodules were found in patients with no outward synptoms of thyroid CA.
     mc.setProbabilities([0.28,0.72])
-    #cy.setProbabilities([0.35,0.65])
-    # pv.setProbabilities([0.12,0.88])
+    im.setProbabilities([0.35,0.65])
     he.setProbabilities([0.15,0.85])
+    # pv.setProbabilities([0.12,0.88])
     # rs.setProbabilities([,])
 
     # set probabilities for nodes with parents
     # These values are made up from but not entirely random
     # They are attempts to estimate what real values might be in a clinical situation
-    ca.setProbabilities([0.8,0.2,0.6,0.4,0.4,0.6,0.1,0.9])
+    ca.setProbabilities([0.88,0.12,0.8,0.2,0.6,0.4,0.7,0.3,0.5,0.5,0.6,0.4,0.3,0.7,0.1,0.9])
     op.setProbabilities([0.95,0.05,0.10,0.90])
     kw.setProbabilities([0.01,0.99,0.30,0.70])
 
@@ -83,7 +83,7 @@ def main():
     # state some evidence and re-examine the beliefs
     tcancer.reset()
     tcancer.setEvidence('mc', 1)
-    tcancer.setEvidence('he', 1)
+    tcancer.setEvidence('im', 1)
     tcancer.computeBeliefs()
     printBeliefs(nodes)
     print ""
